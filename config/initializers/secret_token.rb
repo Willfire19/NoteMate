55080cb71b6b584e096bf8371c22def68b3a0ce8
Willfire19 <willfire19@gmail.com>
2013-08-14 16:00:38 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notemate::Application.config.secret_token = '3149ccd058c4a54db3b3b6c9290aeea3d98aebe84bc17bbe4431d9bdb788a60e20eb09c449273dda4343a2c03fba7de83b1baa38657fe020974e9d899dc8fe6a'
